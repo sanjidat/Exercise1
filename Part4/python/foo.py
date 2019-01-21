83c24acaa6b07d9330d334cfdde29581adef6012
sabrina05 <sabrinc@ifi.uio.no>
2019-01-21 15:52:21 +0100

    python updated
    updated python

--- Part4/python/foo.py
+++ Part4/python/foo.py
@@ -1,36 +1,51 @@
-# Python 3.3.3 and 2.7.6
-# python fo.py
+#	Python Code for Exercise 1:
+
 
 from threading import Thread
 
-
-# Potentially useful thing:
-#   In Python you "import" a global variable, instead of "export"ing it when you declare it
-#   (This is probably an effort to make you feel bad about typing the word "global")
 i = 0
 
+
+
+#Define a function for the thread
 def incrementingFunction():
-    global i
-    # TODO: increment i 1_000_000 times
+    
 
-def decrementingFunction():
-    global i
-    # TODO: decrement i 1_000_000 times
 
+for j in range(1000000):
+        
+	global i
+      
+	i += 1
+
+
+\def decrementingFunction():
+    
+	for j in range(1000000):
+        
+	global i
+     	
+	i -= 1
+
+
+#Create two threads as follows
 
 
 def main():
-    # TODO: Something is missing here (needed to print i)
 
-    incrementing = Thread(target = incrementingFunction, args = (),)
-    decrementing = Thread(target = decrementingFunction, args = (),)
+incrementing = Thread(target = incrementingFunction, args = (),)
+
+decrementing = Thread(target = decrementingFunction, args = (),)
     
-    # TODO: Start both threads
+    
+    incrementing.start()
+    decrementing.start()
     
     incrementing.join()
     decrementing.join()
     
-    print("The magic number is %d" % (i))
-
-
+    print ("The magic number is %d" % (i))
 main()
+
+
+
