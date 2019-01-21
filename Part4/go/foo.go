d1cb546a792321a1ff25c00562b600ef694729a1
sabrina05 <sabrinc@ifi.uio.no>
2019-01-21 16:31:03 +0100

    updated go

--- Part4/go/foo.go
+++ Part4/go/foo.go
@@ -1,4 +1,3 @@
-// Use `go run foo.go` to run your program
 
 package main
 
@@ -12,10 +11,16 @@ var i = 0
 
 func incrementing() {
     //TODO: increment i 1000000 times
+	for j := 0; j < 10000; j++ {
+		i++
+	}
 }
 
 func decrementing() {
     //TODO: decrement i 1000000 times
+	for j := 0; j < 10000; j++ {
+		i--
+	}
 }
 
 func main() {
@@ -23,6 +28,8 @@ func main() {
 	                                    // Try doing the exercise both with and without it!
 
     // TODO: Spawn both functions as goroutines
+	go incrementing()
+    	go decrementing()
 	
     // We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
     // We'll come back to using channels in Exercise 2. For now: Sleep.
